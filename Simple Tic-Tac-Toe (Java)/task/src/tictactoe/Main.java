5b9f409edb788a5511379d98e45bc25f55f5cc1b
Timur Sarsembai <sarsembai.timur@gmail.com>
2025-01-13 12:56:39 +0500

    Final Commit

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -5,19 +5,75 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        int n = 3;
-        char[][] matrix = new char[n][n];
-        int index = 0;
+        char[][] matrix = {
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'}
+        };
 
-        // Заполняем матрицу
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = str.charAt(index++);
-            }
-        }
+        char currentPlayer = 'X';
 
         // Печатаем игровое поле
+        printGameField(matrix);
+
+        // Анализ игрового состояния
+        while(!analyzeGame(matrix)) {
+
+            // Ход игрока
+            userMove(scanner, matrix, currentPlayer);
+
+            // Переключение игрока
+            currentPlayer = switchPlayer(currentPlayer);
+
+            // Печатаем игровое поле
+            printGameField(matrix);
+
+        }
+    }
+
+    public static void userMove(Scanner scanner, char[][] matrix, char currentPlayer) {
+        int x = 0, y = 0;
+
+        while (true) {
+            String usrStep = scanner.nextLine();
+
+            if (isValidInput(usrStep)) {
+                String[] arr = usrStep.split(" ");
+                x = Integer.parseInt(arr[0]);
+                y = Integer.parseInt(arr[1]);
+
+                // Проверка границ
+                if (x > 0 && y > 0 && x <= 3 && y <= 3) {
+                    // Проверка, свободна ли ячейка
+                    if (checkCell(matrix, x - 1, y - 1)) { // Преобразование к индексу массива (0-based)
+                        matrix[x - 1][y - 1] = currentPlayer;
+                        break; // Выход из цикла, если ход выполнен успешно
+                    } else {
+                        System.out.println("This cell is occupied! Choose another one!");
+                    }
+                } else {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                }
+            } else {
+                System.out.println("You should enter numbers!");
+            }
+        }
+    }
+
+    public static char switchPlayer(char currentPlayer) {
+        return (currentPlayer == 'X') ? 'O' : 'X';
+
+    }
+
+    public static boolean isValidInput(String input) {
+        return input.matches("\\d+ \\d+");
+    }
+
+    public static boolean checkCell(char[][] matrix, int x, int y) {
+        return matrix[x][y] == '_';
+    }
+
+    public static void printGameField(char[][] matrix) {
         System.out.println("---------");
         for (char[] row : matrix) {
             System.out.print("| ");
@@ -27,12 +83,9 @@ public class Main {
             System.out.println("|");
         }
         System.out.println("---------");
-
-        // Анализ игрового состояния
-        analyzeGame(matrix);
     }
 
-    public static void analyzeGame(char[][] matrix) {
+    public static boolean analyzeGame(char[][] matrix) {
         int n = matrix.length;
 
         // Подсчитываем количество X и O
@@ -52,14 +105,19 @@ public class Main {
         // Проверка состояния
         if (xWins && oWins || Math.abs(countX - countO) > 1) {
             System.out.println("Impossible");
+            return true;
         } else if (xWins) {
             System.out.println("X wins");
+            return true;
         } else if (oWins) {
             System.out.println("O wins");
+            return true;
         } else if (emptyCells > 0) {
-            System.out.println("Game not finished");
+            // System.out.println("Game not finished");
+            return false;
         } else {
             System.out.println("Draw");
+            return true;
         }
     }
 
